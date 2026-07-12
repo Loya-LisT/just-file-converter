88a0f2df595914d5ef9a5c65e4231255a4c07abb
LoyalisT <loyalist.atlas@gmail.com>
2026-07-12 09:26:45 +0000

    file upload changed
    added new popup dialog window when upload button is pressed

--- JustConverter/script.js
+++ JustConverter/script.js
@@ -1,1 +1,26 @@
-const fileInput = document.getElementById("converter__file-input");
+const FileDialogButton = document.getElementById('upload-button');
+const fileDialog = document.getElementById('file-dialog');
+
+FileDialogButton.addEventListener('click', () => fileDialog.showModal())
+
+//const fileInput = document.getElementById("converter__file-input");
+//const modal = document.getElementById("modal");
+
+/*fileInput.addEventListener("change", function(event) {
+    const selectedFiles = event.target.files; 
+    
+    // Check if a file was actually chosen (handles user hitting 'Cancel') 
+    if (selectedFiles.length > 0) { 
+        // Display file name in the menu 
+        fileNameSpan.textContent = selectedFiles[0].name; 
+        // Show the menu 
+        modal.style.display = 'flex'; 
+    } else { 
+        hideMenu(); 
+    } 
+});
+
+function hideMenu() {
+    modal.style.display = 'none';
+    fileInput.value = '';
+}*/
